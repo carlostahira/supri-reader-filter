9495eba5362859b363f53f80119cde2d637aa255
Carlos T <carlostahira@gmail.com>
2013-08-16 12:32:19 -0300

    original code

---
+++ src/classes/SupriReadFilter.class.php
@@ -0,0 +1,28 @@
+<?php
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . DS . '../PHPExcel/Classes/');
+
+require_once('PHPExcel/Reader/IReadFilter.php');
+
+class SupriReadFilter implements PHPExcel_Reader_IReadFilter {
+	private $_startRow = 0;
+	private $_endRow = 0;
+	private $_columns = array();
+	
+	
+	public function __construct($start, $end, $col) {
+		$this->_startRow = $start;
+		$this->_endRow = $end;
+		$this->_columns = $col;
+	}
+	
+	
+	public function readCell($column, $row, $worksheet = '') {
+		if($row >= $this->_startRow && $row <= $this->_endRow){
+			if(in_array($column, $this->_columns)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
