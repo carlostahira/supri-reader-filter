221400bfa982e03b31ce87683778a10d02f6eca4
Carlos T <carlostahira@gmail.com>
2013-08-20 09:57:26 -0300

    refatorando nome do arquivo e classe de SupriReadFilter para SupriReaderFilter

---
+++ src/classes/SupriReaderFilter.class.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SupriReaderFilter;
+
+class SupriReaderFilter implements PHPExcel_Reader_IReadFilter {
+	private $_startRow = 0;
+	private $_endRow = 0;
+	private $_columns = array();
+	
+	
+	public function __construct($start, $end, $col) {
+		$this->_startRow = $start;
+		$this->_endRow = $end;
+		$this->_columns = $col;
+	}
+	
+	
+	public function readCell($column, $row, $worksheet = '') {
+		if($row >= $this->_startRow && $row <= $this->_endRow){
+			if(in_array($column, $this->_columns)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
